19ecdab3145e6fe5ec2c87bed38e1e2f445a849a
mohtadahmed <mohtadsaeem13@gmail.com>
2022-09-27 00:19:42 +0600

    deposite calculation updated

--- js/deposite.js
+++ js/deposite.js
@@ -1,12 +1,33 @@
 document.getElementById('deposite-button').addEventListener('click', function(){
     // Get the Value from the Deposite input field
     const depositeAmountField = document.getElementById('deposite-amount');
-    const givenDepositeAmount = depositeAmountField.value;
+    const depositeAmountString = depositeAmountField.value;
+    const depositeAmount = parseFloat(depositeAmountString);
     
 
     //Get the Current Deposite Amount
     const currentDepositeAmount = document.getElementById('deposite-total');
-    const depositeAmountValue = currentDepositeAmount.innerText;
-    currentDepositeAmount.innerText = givenDepositeAmount;
+    const previousDepositeAmountString = currentDepositeAmount.innerText;
+    const previousDepositeAmount = parseFloat(previousDepositeAmountString);
     
+
+    // Calculate the deposite value
+    const totalDepositeAmount = depositeAmount + previousDepositeAmount;
+    currentDepositeAmount.innerText = totalDepositeAmount;
+
+    // Get the Total Balance
+    const currentTotal = document.getElementById('total-amount');
+    const previousTotalAmountString = currentTotal.innerText;
+    const previousTotalAmount = parseFloat(previousTotalAmountString);
+    
+    
+
+    // Update Total Amount
+    const totalAmount = depositeAmount + previousTotalAmount;
+    currentTotal.innerText = totalAmount;
+
+
+
+    // Clear the input field
+    depositeAmountField.value = '';
 })
